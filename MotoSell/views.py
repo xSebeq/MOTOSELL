60c7dfd74d648dc1ce2cbbf19cbc5d3273f6f5d7
xSebeq <sebeq245@interia.eu>
2021-05-14 00:51:03 +0200

    WszystkieOpublikowaneOferty view

--- MotoSell/views.py
+++ MotoSell/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Car
+from datetime import datetime
 
-# Create your views here.
+
+class WszystkieOpublikowaneOferty(ListView):
+    model = Car
+    template_name = "MotoSell/opublikowane.html"
+    context_object_name = "cars"
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Car.objects.filter(data_publikacji__lte=datetime.today()).order_by('-data_publikacji')
