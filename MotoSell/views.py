920832f73e84d778d1b00181c4d73016bf554886
xSebeq <sebeq245@interia.eu>
2021-05-14 00:55:08 +0200

    WszystkieOpublikowaneOfertyUzytkownika view

--- MotoSell/views.py
+++ MotoSell/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.contrib.auth.models import User
 from .models import Car
 from datetime import datetime
 
@@ -12,3 +13,14 @@ class WszystkieOpublikowaneOferty(ListView):
 
     def get_queryset(self):
         return Car.objects.filter(data_publikacji__lte=datetime.today()).order_by('-data_publikacji')
+
+
+class WszystkieOpublikowaneOfertyUzytkownika(ListView):
+    model = Car
+    template_name = "MotoSell/opublikowane_uzytkownika.html"
+    context_object_name = "cars"
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user)
+        return Car.objects.filter(uzytkownik = user).order_by('-data_publikacji')
